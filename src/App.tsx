52b8ce2834676366c5d0b63005900b5c4d6f2d1f
Diana-Balaban98 <diana.rud.2012@mail.ru>
2023-11-22 17:23:36 +0300

    install fonts and base styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
 export function App() {
-  return <div>Hello</div>;
+  return (
+    <div
+      style={{ color: "linear-gradient(90deg, #FCB045 36.99%, #FD1D1D 100%)" }}
+    >
+      Hello
+    </div>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,14 @@ import { StrictMode } from "react";
 
 import { createRoot } from "react-dom/client";
 
+import "./styles/index.scss";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/900.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/400.css";
+
 import { App } from "./App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
